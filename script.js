88d848b0a07e47cadbf158563b2ff9a457e4fb31
Alexandr <aksden0@gmail.com>
2023-05-26 19:17:55 +0300

    add index

---
+++ script.js
@@ -0,0 +1,28 @@
+const http = require('hhtp');
+const path = require('path');
+const fs = require('fs');
+const url = require('url');
+
+const dataPath = path.join(__dirname, 'data');
+
+const server = http.createServer((req, res) => {
+    if(req.url == '/jokes' && req.metod == 'GET') {
+        getAllJokes(req, res);
+    }
+});
+
+server.listen(3000);
+
+function getAllJokes(req, res) {
+    let dir = fs.readdirSync(dataPath);
+    let getAllJokes =[];
+    for(let i = 0; i < dir.length; i++) {
+        let file = fs.readFileSync(path.join(dataPath, i+'.json'));
+        let jokeJSON = Buffer.from(file).toString();
+        let joke = JSON.parse(jokeJSON);
+        joke.id = i;
+        
+        allJokes.push(joke);
+    }
+    res.end(JSON.stringify(allJokes));
+}
